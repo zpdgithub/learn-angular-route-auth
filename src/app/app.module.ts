a786760c7c0698a5df114ef17541168f13937a37
peter <peter@soez.io>
2018-04-03 18:02:53 +0800

    初始化

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `(app-root)`,
+})
+export class AppComponent {
+}
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
